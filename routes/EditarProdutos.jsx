eceaa3d040895a7aacbcf511f0b69f99ea309d0e
LuizaNs <luizansjs@gmail.com>
2023-09-01 09:56:27 -0300

    Edição dos links da pasta routes e adição de ícone no cabeçalho.

--- routes/EditarProdutos.jsx
+++
@@ -1,5 +0,0 @@
-export default function EditarProdutos() {
-  return (
-    <div>EditarProdutos</div>
-  )
-}
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from '../routes/Home.jsx'
-import EditarProdutos from '../routes/EditarProdutos.jsx'
-import Erro404 from '../routes/Erro404.jsx'
-import Produtos from '../routes/Produtos.jsx'
+import Home from './routes/Home.jsx'
+import EditarProdutos from './routes/EditarProdutos.jsx'
+import Erro404 from './routes/Erro404.jsx'
+import Produtos from './routes/Produtos.jsx'
 
 const router = createBrowserRouter([ 
-  {path: '/', element: <App/>}
+  {path: '/', element: <App/>, 
+    errorElement: <Erro404></Erro404>,
+    children:[
+      {path: '/', element: <Home/>}, 
+      {path: '/editar/produtos/:id', element: <EditarProdutos/>}, 
+      {path: '/produtos', element: <Produtos/>}, 
+
+  ]}
 ])
 
 
